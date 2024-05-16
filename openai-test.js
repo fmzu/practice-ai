9a273329dce59a0ebdd286b2ca4eafb9e334327a
fmzu <149039422+fmzu@users.noreply.github.com>
2024-05-16 22:02:55 +0900

    update

--- openai-test.js
+++ openai-test.js
@@ -4,11 +4,12 @@ const openai = new OpenAI({ apiKey: "", });
 
 async function main() {
   const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "You are a helpful assistant." }],
-    model: "gpt-3.5-turbo",
+    messages: [{ role: "system", content: "あなたは敬語で話します。" },
+     { role: "user", content: "chatGPTのAPIを使ってWebアプリかbotを作りたいです。Node.jsで作ります。どうやって作ればいいですか？"}],
+    model: "gpt-4o",
   });
 
-  console.log("aaaaaa",completion.choices[0]);
+  console.log("aaaaaa",completion.choices[0].message.content);
 }
 
 main();
